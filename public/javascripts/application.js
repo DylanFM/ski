49f9ec2e2b093bd1c4e6a04b9a7218e4a75de688
DylanFM <dylan.fm@gmail.com>
2010-05-12 23:26:50 +1000

    Just set up raphael canvas and getting a feel for how this could work.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,14 @@
 (function(){
   $(function() {
-    return console.log("Hammertime");
+    var img, paper;
+    paper = Raphael("game", 800, 600);
+    // Create an image to play with
+    img = paper.image("images/squares.png", 365, 530, 70, 70);
+    // Move it
+    // Animate!
+    return img.animate({
+      x: 400,
+      y: 70
+    }, 2000);
   });
 })();
--- src/application.coffee
+++ src/application.coffee
@@ -1,2 +1,9 @@
 $ ->
-  console.log "Hammertime"
+  paper: Raphael "game", 800, 600
+
+  # Create an image to play with
+  img: paper.image "images/squares.png", 365, 530, 70, 70
+
+  # Move it
+  # Animate!
+  img.animate { x: 400, y: 70 }, 2000
