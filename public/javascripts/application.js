29bfe6b82165707b501d0bb44357f945224ad225
DylanFM <dylan.fm@gmail.com>
2010-05-12 23:51:43 +1000

    Moving the little image around.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,40 @@
 (function(){
   $(function() {
-    var img, paper;
+    var img, move, paper;
     paper = Raphael("game", 800, 600);
     // Create an image to play with
     img = paper.image("images/squares.png", 365, 530, 70, 70);
     // Move it
-    // Animate!
-    return img.animate({
-      x: 400,
-      y: 70
-    }, 2000);
+    move = function move(el, direction) {
+      var to;
+      to = {
+        x: el.attrs.x,
+        y: el.attrs.y
+      };
+      if (direction === "left") {
+        to.x = 0;
+      } else if (direction === "up") {
+        to.y = 70;
+      } else if (direction === "down") {
+        to.y = 530;
+      } else if (direction === "right") {
+        to.x = 730;
+        // Animate!
+      }
+      return el.animate(to, 2000);
+    };
+    // Let's get it moving around with arrow keys
+    return $("html").keydown(function(event) {
+      var _a;
+      if ((_a = event.keyCode) === 37) {
+        return move(img, "left");
+      } else if (_a === 38) {
+        return move(img, "up");
+      } else if (_a === 39) {
+        return move(img, "right");
+      } else if (_a === 40) {
+        return move(img, "down");
+      }
+    });
   });
 })();
--- src/application.coffee
+++ src/application.coffee
@@ -5,5 +5,24 @@ $ ->
   img: paper.image "images/squares.png", 365, 530, 70, 70
 
   # Move it
-  # Animate!
-  img.animate { x: 400, y: 70 }, 2000
+  move: (el, direction) ->
+    to: { x: el.attrs.x, y: el.attrs.y }
+    switch direction
+      when "left"
+        to.x = 0
+      when "up"
+        to.y = 70
+      when "down"
+        to.y = 530
+      when "right"
+        to.x = 730
+    # Animate!
+    el.animate to, 2000
+
+  # Let's get it moving around with arrow keys
+  $("html").keydown (event) ->
+    switch event.keyCode
+      when 37 then move img, "left"
+      when 38 then move img, "up"
+      when 39 then move img, "right"
+      when 40 then move img, "down"
