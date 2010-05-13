eebef80d8933502df3b092b2fbae3cdd1ee7107a
DylanFM <dylan.fm@gmail.com>
2010-05-13 23:30:42 +1000

    Got something that resembles a skiier going something that resembles a hill.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,11 @@
 (function(){
   $(function() {
-    var img, move, paper, person, point;
+    var bg, move, paper, person;
     paper = Raphael("game", 800, 600);
     // Create an image to play with
-    img = paper.image("images/squares.png", 365, 530, 70, 70);
+    bg = paper.image("images/background.png", -400, 0, 1600, 3000);
     // Make a representation of a person
-    person = paper.image("images/person.png", 390, 285, 20, 30);
+    person = paper.image("images/person.png", 390, 285, 20, 40);
     // Move it
     move = function move(el, direction) {
       var to;
@@ -13,44 +13,30 @@
         x: el.attrs.x,
         y: el.attrs.y
       };
+      // Let's hard code in a going down
+      to.y = -2400;
       if (direction === "left") {
-        to.x = 0;
+        to.x = -800;
       } else if (direction === "up") {
-        to.y = 70;
-      } else if (direction === "down") {
-        to.y = 530;
+        // to.y = -2400
       } else if (direction === "right") {
-        to.x = 730;
+        to.x = 0;
         // Animate!
       }
-      return el.animate(to, 2000);
-    };
-    point = function point(el, direction) {
-      if (direction === "left") {
-        return el.rotate(270, true);
-      } else if (direction === "up") {
-        return el.rotate(0, true);
-      } else if (direction === "down") {
-        return el.rotate(180, true);
-      } else if (direction === "right") {
-        return el.rotate(90, true);
-      }
+      return el.animate(to, 5000);
     };
     // Let's get it moving around with arrow keys
     return $("html").keydown(function(event) {
       var _a;
       if ((_a = event.keyCode) === 37) {
-        move(img, "right");
-        point(person, "left");
-      } else if (_a === 38) {
-        move(img, "down");
-        point(person, "up");
+        move(bg, "right");
+        console.log("Turning left");
       } else if (_a === 39) {
-        move(img, "left");
-        point(person, "right");
+        move(bg, "left");
+        console.log("Turning right");
       } else if (_a === 40) {
-        move(img, "up");
-        point(person, "down");
+        move(bg, "up");
+        console.log("Straight down!");
       }
       return false;
     });
--- src/application.coffee
+++ src/application.coffee
@@ -2,50 +2,36 @@ $ ->
   paper: Raphael "game", 800, 600
 
   # Create an image to play with
-  img: paper.image "images/squares.png", 365, 530, 70, 70
+  bg: paper.image "images/background.png", -400, 0, 1600, 3000
 
   # Make a representation of a person
-  person: paper.image "images/person.png", 390, 285, 20, 30
+  person: paper.image "images/person.png", 390, 285, 20, 40
 
   # Move it
   move: (el, direction) ->
     to: { x: el.attrs.x, y: el.attrs.y }
+    # Let's hard code in a going down
+    to.y = -2400
     switch direction
       when "left"
+        to.x = -800
+      when "up"
+        # to.y = -2400
+      when "right"
         to.x = 0
-      when "up"
-        to.y = 70
-      when "down"
-        to.y = 530
-      when "right"
-        to.x = 730
     # Animate!
-    el.animate to, 2000
-
-  point: (el, direction) ->
-    switch direction
-      when "left"
-        el.rotate 270, true
-      when "up"
-        el.rotate 0, true
-      when "down"
-        el.rotate 180, true
-      when "right"
-        el.rotate 90, true
+    el.animate to, 5000
 
   # Let's get it moving around with arrow keys
   $("html").keydown (event) ->
     switch event.keyCode
       when 37
-        move img, "right"
-        point person, "left"
-      when 38
-        move img, "down"
-        point person, "up"
+        move bg, "right"
+        console.log "Turning left"
       when 39
-        move img, "left"
-        point person, "right"
+        move bg, "left"
+        console.log "Turning right"
       when 40
-        move img, "up"
-        point person, "down"
+        move bg, "up"
+        console.log "Straight down!"
     false
